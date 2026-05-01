666d26275eb868ee1942a618590dbdd5abae6266
Allari Snehith <allarisnehith@gmail.com>
2026-05-01 21:57:13 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000/api' });
+const API = axios.create({ 
+  baseURL: import.meta.env.VITE_API_URL 
+});
 
 // Attach JWT token to every request automatically
 API.interceptors.request.use((config) => {
